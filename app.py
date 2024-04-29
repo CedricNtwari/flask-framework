6bf43e32a8aa9a00406807a66f915687f59a8f58
CedricNtwari <cedric.ntwari@powercoders.org>
2024-04-29 22:40:47 +0200

    use base template to navigate pages

--- app.py
+++ app.py
@@ -15,5 +15,9 @@ def about():
 def contact():
     return render_template('contact.html')
 
+@app.route('/career')
+def career():
+    return render_template('careers.html')
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP', '0.0.0.0'), port=int(os.environ.get('PORT', 8080)), debug=True)
