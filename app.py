9d6bf39f9484cb6ac123f29c35783fb43060392f
CedricNtwari <cedric.ntwari@powercoders.org>
2024-04-30 19:04:09 +0200

    Passing Data From A View To A Template

--- app.py
+++ app.py
@@ -9,15 +9,15 @@ def index():
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', page_title="About")
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('contact.html',  page_title="Contact")
 
 @app.route('/career')
 def career():
-    return render_template('careers.html')
+    return render_template('careers.html',  page_title="Career")
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP', '0.0.0.0'), port=int(os.environ.get('PORT', 8080)), debug=True)
